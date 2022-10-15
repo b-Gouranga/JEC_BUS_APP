010e5f77521b823b78a1a29e01ebc4d7c70627ee
Chinmoy-Bora <chinmoyborah100@gmail.com>
2022-10-15 21:16:38 +0530

    splashscreen

---
+++ app/src/main/java/com/hi/composeapp/splashscreen.kt
@@ -0,0 +1,91 @@
+package com.hi.composeapp
+
+import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.jecbusservice.R
+import kotlinx.coroutines.delay
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
+                Navigation()
+            }
+        }
+    }
+}
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController,
+        startDestination = "splash_screen") {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+
+        // Main Screen
+        composable("main_screen") {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "", color = Color.Black, fontSize = 24.sp)
+            }
+        }
+    }
+}
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    // Animation
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.7f,
+            // tween Animation
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                }))
+        // Customize the delay time
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+
+    // Image
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()) {
+        // Change the logo
+        Image(painter = painterResource(id = R.drawable.bus1),
+            contentDescription = "LOGO",
+            //modifier = Modifier.scale(scale.value))
+            modifier= Modifier.size(200.dp))
+        //Text(text = "JEC BUS SERVICE", color = Color.Black, fontSize = 24.sp,TextAlign= c)
+    }
+}
